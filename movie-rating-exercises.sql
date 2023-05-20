c1deb98b9248f0c28347a59caa4c6d1b6ab52d80
spookybart101 <bartoverduin@hotmail.nl>
2023-05-20 23:59:28 +0200

    Update movie-rating-exercises.sql

--- movie-rating-exercises.sql
+++ movie-rating-exercises.sql
@@ -55,7 +55,7 @@ ORDER BY Spread DESC, Title;
 
 -- 9. Find the difference between the average rating of movies released before 1980 and the average rating of movies released after 1980. (Make sure to calculate the average rating for each movie, then the average of those averages for movies before 1980 and movies after. Don't just calculate the overall average rating before and after 1980.) 
 SELECT AVG(S1) - AVG(S2) 
-FROM(
+FROM (
 	SELECT AVG(STARS) S1 
 	FROM Movie M, Rating R 
 	WHERE M.MID=R.MID and year < 1980
